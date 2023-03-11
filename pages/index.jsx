5adbf00a648afd13161297fd9091e1541944320a
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 13:01:47 +0000

    removing html inline styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,23 +4,25 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home(initialData) {
-  let height = 16;
+  let containerHeight = '30em';
+  let rowHeight = '15em';
+  let cellHeight = '16em';
   return (    
     <SimpleLayout>
-      <div class="container" style="height: 30em;"> 
-      <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: height + 'em'}}>
+      <div class="container" style={{height: containerHeight}}> 
+      <div class="row" style={{height: rowHeight}}> 
+        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: cellHeight}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: height + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: cellHeight}}>
           
         </div>
       </div>
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-success" style={{height: height + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-success" style={{height: cellHeight}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: height + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: cellHeight}}>
            
         </div>
       </div>
