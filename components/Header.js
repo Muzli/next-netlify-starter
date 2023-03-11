234f3a0e11de6e35cad2912283b03a99cdca9fd3
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 10:18:47 +0000

    First version of formverk header

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,14 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return <div class="navbar navbar-light bg-light box-shadow">
+          <div class="container d-flex justify-content-between">
+            <a href="#" class="navbar-brand d-flex align-items-center">
+              <div class="h-50">
+                Formverk
+              </div>
+            </a>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </div>
 }
