7eaad94a95abf5da92ad8f664def214ecdcfb92d
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 12:56:18 +0000

    correct style usage for jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,18 +8,18 @@ export default function Home(initialData) {
     <SimpleLayout>
       <div class="container" style="height: 30em;"> 
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-primary" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: 16 + 'em'}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-secondary" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: 16 + 'em'}}>
           
         </div>
       </div>
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-success" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-success" style={{height: 16 + 'em'}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-danger" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: 16 + 'em'}}>
            
         </div>
       </div>
