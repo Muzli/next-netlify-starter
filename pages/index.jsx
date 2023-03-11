d25fe17e9e2fd77270dd093cbb141fb7378bfc12
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 12:58:32 +0000

    jsx styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,22 +4,23 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home(initialData) {
+  let height = 16;
   return (    
     <SimpleLayout>
       <div class="container" style="height: 30em;"> 
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: 16 + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: height + 'em'}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: 16 + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: height + 'em'}}>
           
         </div>
       </div>
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-success" style={{height: 16 + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-success" style={{height: height + 'em'}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: 16 + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: height + 'em'}}>
            
         </div>
       </div>
