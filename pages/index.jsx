158b2ad4d4d89b54ffbf02b73a450b69cae6fb33
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 13:03:44 +0000

    testing

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,25 +4,25 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home(initialData) {
-  let containerHeight = '30em';
-  let rowHeight = '15em';
-  let cellHeight = '16em';
+  let containerHeight = 30;
+  let rowHeight = 15;
+  let cellHeight = 16;
   return (    
     <SimpleLayout>
-      <div class="container" style={{height: containerHeight}}> 
-      <div class="row" style={{height: rowHeight}}> 
-        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: cellHeight}}>
+      <div class="container" style={{height: containerHeight + 'em'}}> 
+      <div class="row" style={{height: rowHeight + 'em'}}> 
+        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: cellHeight + 'em'}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: cellHeight}}>
+        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: cellHeight + 'em'}}>
           
         </div>
       </div>
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-success" style={{height: cellHeight}}>
+        <div class="col-xs-12 col-xl-6 bg-success" style={{height: cellHeight + 'em'}}>
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: cellHeight}}>
+        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: cellHeight + 'em'}}>
            
         </div>
       </div>
