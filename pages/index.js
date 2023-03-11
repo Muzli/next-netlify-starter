e539772dae0596072595da99d381b23ae4374a61
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 10:30:48 +0000

    Finding right place for header

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
+  return (    
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
+      <Header />
+      <main>        
         <p className="description">
           Got started by editing <code>pages/index.js</code>
         </p>
