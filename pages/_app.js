7be8a8bf4c96a18c321126f9bac4f5977f8cfb21
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 09:54:35 +0000

    Adding bootstrap to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import '@styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+import '@styles/globals.css';
+import { useEffect } from "react";
 
 function Application({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  
   return <Component {...pageProps} />
 }
 
