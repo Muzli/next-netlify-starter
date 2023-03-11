e6bf9222afe87df1ccfed6d87f502589d2273bad
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 12:53:38 +0000

    Removing bacground class to find error

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,18 +8,18 @@ export default function Home(initialData) {
     <SimpleLayout>
       <div class="container" style="height: 30em;"> 
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-primary paint-background-img" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-primary" style="height: 16 em;">
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-secondary garage-background-img" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-secondary" style="height: 16 em;">
           
         </div>
       </div>
       <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-success spoiler-background-img" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-success" style="height: 16 em;">
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-danger lc-background-img" style="height: 16 em;">
+        <div class="col-xs-12 col-xl-6 bg-danger" style="height: 16 em;">
            
         </div>
       </div>
