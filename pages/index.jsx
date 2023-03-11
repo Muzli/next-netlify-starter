7cd797d0dc99f67d33a6b5b5a61595601187e009
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 13:09:22 +0000

    now using classes instead of inline styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,25 +4,22 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home(initialData) {
-  let containerHeight = '30';
-  let rowHeight = '15';
-  let cellHeight = '16';
   return (    
     <SimpleLayout>
-      <div class="container" style={{height: containerHeight + 'em'}}> 
-      <div class="row" style={{height: rowHeight + 'em'}}> 
-        <div class="col-xs-12 col-xl-6 bg-primary" style={{height: cellHeight + 'em'}}>
+      <div class="container"> 
+      <div class="row default-row-height"> 
+        <div class="col-xs-12 col-xl-6 bg-primary default-cell-height">
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-secondary" style={{height: cellHeight + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-secondary default-cell-height">
           
         </div>
       </div>
-      <div class="row" style="height: 15em;"> 
-        <div class="col-xs-12 col-xl-6 bg-success" style={{height: cellHeight + 'em'}}>
+      <div class="row default-row-height"> 
+        <div class="col-xs-12 col-xl-6 bg-success default-cell-height">
           
         </div>
-        <div class="col-xs-12 col-xl-6 bg-danger" style={{height: cellHeight + 'em'}}>
+        <div class="col-xs-12 col-xl-6 bg-danger default-cell-height">
            
         </div>
       </div>
