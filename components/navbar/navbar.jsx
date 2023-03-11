8005a34f01d30d175b4bf30c0de6597a0d7d59ca
Elvar Helgason <elvarhelga@gmail.com>
2023-03-11 12:27:26 +0000

    Sizing image to fit navbar

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -8,9 +8,7 @@ export default function Navbar() {
         <div className="container-xl">
             <Link href="/">
                 <a className="navbar-brand">
-                    <div class="h-50">
-                        <img src="/formverk_logo.png" alt="Formverk merki"/>
-                    </div>
+                    <img class="img-fluid" src="/formverk_logo.png" alt="Formverk merki"/>
                 </a>
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
