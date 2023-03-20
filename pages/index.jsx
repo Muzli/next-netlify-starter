673d03e9aa2c47f92d5287c307e441c4d4644d5e
Elvar Helgason <elvarhelga@gmail.com>
2023-03-20 18:26:59 +0000

    Grid now has faded backgrounds and bold white text

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,26 +10,26 @@ export default function Home(initialData) {
     <SimpleLayout>
     <Container> 
         <Row> 
-          <Col className="d-flex col-xs-12 col-l-6 bg-primary default-cell-height garage-background-img justify-content-center align-items-center">
-            <div className="display-1">
-              Réttingar
+          <Col className="d-flex col-xs-12 col-l-6 bg-secondary default-cell-height garage-background-img justify-content-center align-items-center">
+            <div className="display-2 text-white">
+              <strong>Réttingar</strong>
             </div>
           </Col>
           <Col className="d-flex col-xs-12 col-l-6 bg-secondary default-cell-height paint-background-img justify-content-center align-items-center">
-            <div  className="display-2">
-              Sprautun
+            <div className="display-2 text-white">
+              <strong>Sprautun</strong>
             </div>
           </Col>
         </Row>
         <Row> 
-          <Col className="d-flex col-xs-12 col-l-6 bg-success default-cell-height lc-background-img justify-content-center align-items-center">
-            <div  className="display-3">
-              Brettakantar
+          <Col className="d-flex col-xs-12 col-l-6 bg-secondary default-cell-height lc-background-img justify-content-center align-items-center">
+            <div className="display-2 text-white">
+              <strong>Brettakantar</strong>
             </div>
           </Col>
-          <Col className="d-flex col-xs-12 col-l-6 bg-danger default-cell-height spoiler-background-img justify-content-center align-items-center">
-            <div  className="display-4">
-              Formgerð
+          <Col className="d-flex col-xs-12 col-l-6 bg-secondary   default-cell-height spoiler-background-img justify-content-center align-items-center">
+            <div className="display-2 text-white">
+              <strong>Formgerð</strong>
             </div>
           </Col>
         </Row>
