badb5e9f1224952c5f4be6cd5d37c95eb3afe068
Elvar Helgason <elvarhelga@gmail.com>
2023-04-03 13:16:14 +0000

    Removed unnecessary double container

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,43 +7,41 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Home(initialData) {
   return (    
-    <SimpleLayout>
-    <Container> 
-        <Row>
-          <Col className="d-flex col-12 col-lg-6 bg-secondary default-cell-height lc-background-img justify-content-center align-items-center position-relative">
-            <div className="display-2 text-white">
-              <a className="stretched-link index-link" href="/brettakantar"><strong>Brettakantar</strong></a>
-            </div>
-          </Col>
-          <Col className="d-flex col-12 col-lg-6 bg-secondary default-cell-height garage-background-img justify-content-center align-items-center position-relative">
-            <div className="display-2 text-white">
-              <a className="stretched-link index-link" href="/tjonaskodun"><strong>Tjónaskoðun</strong></a>
-            </div>
-          </Col>          
-        </Row>        
-        <Row>
-          <Col id="about" className="d-flex col-12 justify-content-center align-items-center mt-3">
-            <Row>
-              <Col className="col-12 text-center">
-                Formverk ehf. Bæjarflöt 6, 112 Reykjavík
-              </Col>
-              <Col className="col-12 text-center">
-                Sími: 567 7660 / 898 6471
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="d-flex col-12 justify-content-center align-items-center">
-            <a href="https://ja.is/kort/?d=hashid%3AxdLZX&x=363571&y=407520&type=map&nz=13.70" target="_blank">Staðsetning á korti já.is</a>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="d-flex col-12 justify-content-center align-items-center">
-            <p>Opnunartími</p>
-          </Col>
-        </Row>
-      </Container>
+    <SimpleLayout>    
+      <Row>
+        <Col className="d-flex col-12 col-lg-6 bg-secondary default-cell-height lc-background-img justify-content-center align-items-center position-relative">
+          <div className="display-2 text-white">
+            <a className="stretched-link index-link" href="/brettakantar"><strong>Brettakantar</strong></a>
+          </div>
+        </Col>
+        <Col className="d-flex col-12 col-lg-6 bg-secondary default-cell-height garage-background-img justify-content-center align-items-center position-relative">
+          <div className="display-2 text-white">
+            <a className="stretched-link index-link" href="/tjonaskodun"><strong>Tjónaskoðun</strong></a>
+          </div>
+        </Col>          
+      </Row>        
+      <Row>
+        <Col id="about" className="d-flex col-12 justify-content-center align-items-center mt-3">
+          <Row>
+            <Col className="col-12 text-center">
+              Formverk ehf. Bæjarflöt 6, 112 Reykjavík
+            </Col>
+            <Col className="col-12 text-center">
+              Sími: 567 7660 / 898 6471
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex col-12 justify-content-center align-items-center">
+          <a href="https://ja.is/kort/?d=hashid%3AxdLZX&x=363571&y=407520&type=map&nz=13.70" target="_blank">Staðsetning á korti já.is</a>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex col-12 justify-content-center align-items-center">
+          <p>Opnunartími</p>
+        </Col>
+      </Row>
     </SimpleLayout>
   )
 }
