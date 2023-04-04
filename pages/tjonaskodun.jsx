4e640324768f1f750bd9e416b7fbc03b68d901f9
Elvar Helgason <elvarhelga@gmail.com>
2023-04-04 20:33:55 +0000

    Simple template for lvl-2 page

--- pages/tjonaskodun.jsx
+++ pages/tjonaskodun.jsx
@@ -1,16 +1,23 @@
-import Link from 'next/link'
 import SimpleLayout from '../components/layout/simple'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 export default function Tjonaskodun(initialData) {
   return (
     <SimpleLayout>
-      <Container>
-        <h1>Réttingar</h1>
-        <p>Einhver texti</p>
-        <h1>Sprautun</h1>
-        <p>Einhver texti</p>
-      </Container>
+      <Row>
+        <Col className='d-flex col-12 col-lg-6 position-relative'>
+            <div className="align-items-center justify-content-center">
+              <h1 className="display-2">Réttingar</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere posuere dui. Proin non porta est. Praesent a lacus vestibulum, laoreet elit mattis, rutrum mauris. Aliquam urna massa, tempus vel fermentum pulvinar, feugiat porttitor nibh.</p>
+            </div>
+        </Col>
+        <Col className='d-flex col-12 col-lg-6 position-relative'>
+            <div className="align-items-center justify-content-center">
+              <h1 className="display-2">Sprautun</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere posuere dui. Proin non porta est. Praesent a lacus vestibulum, laoreet elit mattis, rutrum mauris. Aliquam urna massa, tempus vel fermentum pulvinar, feugiat porttitor nibh. Integer condimentum magna non odio scelerisque congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc sapien libero, suscipit et orci vel, elementum laoreet magna.</p>
+            </div>
+        </Col>
+      </Row>
     </SimpleLayout>
   )
 }
