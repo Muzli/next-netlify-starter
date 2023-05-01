ec5a2493b81bdebb46bcaf46cadaf9747018ffbc
Elvar Helgason <elvarhelga@gmail.com>
2023-05-01 11:29:12 +0000

    Side layout in comments

--- pages/brettakantar.jsx
+++ pages/brettakantar.jsx
@@ -1,6 +1,16 @@
 import SimpleLayout from '../components/layout/simple'
 import { Container, Row, Col } from 'react-bootstrap';
 
+// Vera með 2 dropdown lista vinstra megin
+// Annar fyrir bílgerð
+// Hinn fyrir dekkjastærð
+
+// Vera með 2 lista af anchor links undir Brettakantar
+// Annar fyrir bílgerð
+// Hinn fyrir dekkjastærð
+// Þegar smellt er á atriði í öðrum hvorum listanum 
+// þá filterast hinn listinn eftir þeim vörum sem eru í boði
+
 export default function Brettakantar() {
   return (
     <SimpleLayout>
