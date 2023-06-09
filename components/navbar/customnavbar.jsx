67a2347b12e3f3b1ad23daee5669969953eac7cd
Elvar Helgason <elvarhelga@gmail.com>
2023-06-09 09:21:16 +0000

    Added direct links to manufacturers

--- components/navbar/customnavbar.jsx
+++ components/navbar/customnavbar.jsx
@@ -1,4 +1,4 @@
-import { Nav, Navbar, Image } from 'react-bootstrap';
+import { Nav, Navbar, Image, NavDropdown } from 'react-bootstrap';
 import { React } from 'react'
 
 // See https://react-bootstrap.netlify.app/components/navbar/
@@ -22,6 +22,23 @@ function CustomNavbar() {
       <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
+          <NavDropdown title="Brettakantar" id="collasible-nav-dropdown">
+            <NavDropdown.Item href='/catalog_dodge'>Dodge</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_chevrolet'>Chevrolet</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_jeep'>Jeep</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_ford'>Ford</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_musso'>Musso</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_isuzu'>Isuzu</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_landrover'>Land Rover</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_iveco'>Iveco</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_toyota'>Toyota</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_nissan'>Nissan</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_mitsubishi'>Mitsubishi</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_benz'>Mercedes Benz</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_suzuki'>Suzuki</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_volkswagen'>Volkswagen</NavDropdown.Item>
+            <NavDropdown.Item href='/catalog_scaut'>Scaut</NavDropdown.Item>
+          </NavDropdown>
           <Nav.Link href="/brettakantar">Brettakantar</Nav.Link>
           <Nav.Link href="/tjonaskodun">Tjónaskoðun</Nav.Link>
           <Nav.Link href="#about">Um okkur</Nav.Link>
