d8a321b40b7d3de1ffdd3013941e5dd10afca403
ElvarHelga <elvarhelga@gmail.com>
2024-02-04 18:47:41 +0000

    Z-orderin plus fade
    Inactive images are now rendered faded, behind the active image

--- components/carousel/heroCarousel.jsx
+++ components/carousel/heroCarousel.jsx
@@ -6,17 +6,12 @@ function HeroCarousel() {
         <Carousel>
             <Carousel.Item>
                 <CarouselImage src='backgrounds/expo/Akur 1.jpeg'/>
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <CarouselImage src='backgrounds/expo/Benz 1.jpeg'/>
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Bronco 2.jpeg'/>
             </Carousel.Item>
         </Carousel>
     );
