e1812cc0fc890ce112a88d968dde3bc3d516d110
ElvarHelga <elvarhelga@gmail.com>
2024-02-12 19:14:18 +0000

    removing caption

--- components/carousel/heroCarousel.jsx
+++ components/carousel/heroCarousel.jsx
@@ -6,17 +6,11 @@ function HeroCarousel() {
         <Carousel>
             <Carousel.Item>
                 <CarouselImage src='backgrounds/expo/Akur 1.jpeg'/>
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                
             </Carousel.Item>
             <Carousel.Item>
                 <CarouselImage src='backgrounds/expo/Benz 1.jpeg'/>
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                
             </Carousel.Item>
         </Carousel>
     );
