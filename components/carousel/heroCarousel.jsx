714dc3c8f1627be2089c85655de0fa591e7aa054
ElvarHelga <elvarhelga@gmail.com>
2024-02-12 19:38:06 +0000

    All images loaded in carousel
    Also added fade effect to carousel

--- components/carousel/heroCarousel.jsx
+++ components/carousel/heroCarousel.jsx
@@ -3,14 +3,51 @@ import CarouselImage from './carouselImage';
 
 function HeroCarousel() {
     return (
-        <Carousel>
+        <Carousel fade>
             <Carousel.Item>
-                <CarouselImage src='backgrounds/expo/Akur 1.jpeg'/>
-                
+                <CarouselImage src='backgrounds/expo/Akur 1.jpeg'/>                
             </Carousel.Item>
             <Carousel.Item>
-                <CarouselImage src='backgrounds/expo/Benz 1.jpeg'/>
-                
+                <CarouselImage src='backgrounds/expo/Benz 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Bronco 2.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Bronco 3.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Cherokee 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Expedition 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Ford 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Hilux 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Iveco 3.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Jeep 2.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Jimny 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Land Cruiser 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Patrol 3.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Rubicon 1.jpeg'/>                
+            </Carousel.Item>
+            <Carousel.Item>
+                <CarouselImage src='backgrounds/expo/Toyota 1.jpeg'/>                
             </Carousel.Item>
         </Carousel>
     );
