9a096b0a2ba1c239df20aa25d255db58722f0945
ElvarHelga <elvarhelga@gmail.com>
2024-03-03 17:26:55 +0000

    Added padding

--- pages/tjonaskodun.jsx
+++ pages/tjonaskodun.jsx
@@ -6,13 +6,13 @@ export default function Tjonaskodun(initialData) {
     <SSRProvider>
       <SimpleLayout>
         <Row>
-          <Col className='d-flex col-12 col-lg-6 position-relative'>
+          <Col className='d-flex col-12 col-lg-6 position-relative px-5'>
               <div className="align-items-center justify-content-center">
                 <h1 className="display-2">Réttingar</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere posuere dui. Proin non porta est. Praesent a lacus vestibulum, laoreet elit mattis, rutrum mauris. Aliquam urna massa, tempus vel fermentum pulvinar, feugiat porttitor nibh.</p>
               </div>
           </Col>
-          <Col className='d-flex col-12 col-lg-6 position-relative'>
+          <Col className='d-flex col-12 col-lg-6 position-relative px-5'>
               <div className="align-items-center justify-content-center">
                 <h1 className="display-2">Sprautun</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere posuere dui. Proin non porta est. Praesent a lacus vestibulum, laoreet elit mattis, rutrum mauris. Aliquam urna massa, tempus vel fermentum pulvinar, feugiat porttitor nibh. Integer condimentum magna non odio scelerisque congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc sapien libero, suscipit et orci vel, elementum laoreet magna.</p>
