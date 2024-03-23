1db94116f5f8ab36f62d9b750be7d8fdd807c0dc
ElvarHelga <elvarhelga@gmail.com>
2024-03-23 15:38:59 +0000

    Gallery v1 plus navbar background

--- components/imagegallery/imageGallery.jsx
+++ components/imagegallery/imageGallery.jsx
@@ -1,27 +1,143 @@
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 function ImageGallery() {
     return (
-        <Row>
-            <Col className="col-lg-4 col-md-12 mb-4 mb-lg-0">
-                <img src="backgrounds/expo/Akur 1.jpeg"
-                    className="w-100 shadow-1-strong rounded mb-4"/>
-                <img src="backgrounds/expo/Benz 1.jpeg"
-                    className="w-100 shadow-1-strong rounded mb-4"/>
-            </Col>
-            <Col className="col-lg-4 mb-4 mb-lg-0">
-                <img src="backgrounds/expo/Bronco 1.jpeg"
-                    className="w-100 shadow-1-strong rounded mb-4"/>
-                <img src="backgrounds/expo/Bronco 2.jpeg"
-                    className="w-100 shadow-1-strong rounded mb-4"/>
-            </Col>
-            <Col className="col-lg-4 mb-4 mb-lg-0">
-                <img src="backgrounds/expo/Bronco 3.jpeg"
-                    className="w-100 shadow-1-strong rounded mb-4"/>
-                <img src="backgrounds/expo/Cherokee 1.jpeg"
-                    className="w-100 shadow-1-strong rounded mb-4"/>
-            </Col>
-        </Row>
+        <Container>
+            <Row>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Akur 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Benz 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Bronco 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Bronco 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Bronco 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Cherokee 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Expedition 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Expedition 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Expedition 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Expedition 4.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Ford 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Ford 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Ford 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Ford 4.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Ford 5.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Ford 6.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Hilux 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Hilux 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Hilux 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Iveco 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Iveco 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Iveco 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Jeep 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Jeep 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Jimny 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Kerra 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Land Cruiser 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Land Cruiser 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Land Cruiser 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Patrol 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Patrol 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Patrol 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Rubicon 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Rubicon 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Rubicon 3.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Rubicon 4.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Rubicon 5.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Rubicon 6.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+                <Col className="col-lg-4 col-12 mb-4 mb-lg-0">
+                    <img src="backgrounds/expo/gallery/Toyota 1.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                    <img src="backgrounds/expo/gallery/Toyota 2.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"/>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
