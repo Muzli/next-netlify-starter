9553a1f3b1c7599d72fef7445eb9bc5cb7c18348
ElvarHelga <elvarhelga@gmail.com>
2024-06-27 19:45:17 +0000

    Added Icelandic text to tjonaskodun.jsx

--- pages/tjonaskodun.jsx
+++ pages/tjonaskodun.jsx
@@ -11,16 +11,10 @@ export default function Tjonaskodun() {
   return (
     <SimpleLayout>
       <Row>
-        <Col className='d-flex col-12 col-lg-6 position-relative px-5'>
-            <div className="align-items-center justify-content-center">
-              <h1 className="display-2">{Dictionary()[language]["damage_inspection"]["corrections"]}</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere posuere dui. Proin non porta est. Praesent a lacus vestibulum, laoreet elit mattis, rutrum mauris. Aliquam urna massa, tempus vel fermentum pulvinar, feugiat porttitor nibh.</p>
-            </div>
-        </Col>
-        <Col className='d-flex col-12 col-lg-6 position-relative px-5'>
-            <div className="align-items-center justify-content-center">
-              <h1 className="display-2">{Dictionary()[language]["damage_inspection"]["painting"]}</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere posuere dui. Proin non porta est. Praesent a lacus vestibulum, laoreet elit mattis, rutrum mauris. Aliquam urna massa, tempus vel fermentum pulvinar, feugiat porttitor nibh. Integer condimentum magna non odio scelerisque congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc sapien libero, suscipit et orci vel, elementum laoreet magna.</p>
+        <Col className='px-5'>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '60vh' }}>
+              <h1 className="display-2 py-3">{Dictionary()[language]["navbar"]["inspection"]}</h1>
+              <p>Þú getur komið við hjá okkur í tjónaskoðun til að fá umsögn og kostnaðarmat vegna viðgerðar eða tjóns sem þú hefur orðið fyrir.</p>
             </div>
         </Col>
       </Row>
