fed641c15b3579e5e2df0dfe159a434d79516a9b
ElvarHelga <elvarhelga@gmail.com>
2025-01-18 17:44:56 +0000

    Another attempt

--- lib/getFilenames/getFilenames.jsx
+++ lib/getFilenames/getFilenames.jsx
@@ -3,11 +3,12 @@ import path from 'path';
 
 export function getFilenames(directory) {
   console.log('process.cwd()', process.cwd());
-  var server_dir = path.join(process.cwd(), '.next', 'server');
-  var dirs = fs.readdirSync(server_dir).filter(file => fs.lstatSync(path.join(server_dir, file)).isDirectory());
-  var files = fs.readdirSync(server_dir).filter(file => fs.lstatSync(path.join(server_dir, file)).isFile());
+  var testDirPath = path.join(__dirname, '..', '..', 'public', directory);
+  var dirs = fs.readdirSync(testDirPath).filter(file => fs.lstatSync(path.join(testDirPath, file)).isDirectory());
+  var files = fs.readdirSync(testDirPath).filter(file => fs.lstatSync(path.join(testDirPath, file)).isFile());
   console.log('contents dirs', dirs);
   console.log('contents files', files);
+  
   
   const dirPath = path.join(process.cwd(), directory);
   return fs.readdirSync(dirPath).filter(file => file.endsWith('.jpeg') || file.endsWith('.jpg') || file.endsWith('.png'));
