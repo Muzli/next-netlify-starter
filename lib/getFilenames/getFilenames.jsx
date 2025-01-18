e3f4784d17e7df86420f33eddb172d945e4030c0
ElvarHelga <elvarhelga@gmail.com>
2025-01-18 17:28:40 +0000

    Now logging the .next/server directory

--- lib/getFilenames/getFilenames.jsx
+++ lib/getFilenames/getFilenames.jsx
@@ -3,8 +3,11 @@ import path from 'path';
 
 export function getFilenames(directory) {
   console.log('process.cwd()', process.cwd());
-  var dirs = fs.readdirSync(process.cwd()).filter(file => fs.lstatSync(path.join(process.cwd(), file)).isDirectory());
-  console.log('contents', dirs);
+  var server_dir = path.join(process.cwd(), '.next', 'server');
+  var dirs = fs.readdirSync(server_dir).filter(file => fs.lstatSync(path.join(server_dir, file)).isDirectory());
+  var files = fs.readdirSync(server_dir).filter(file => fs.lstatSync(path.join(server_dir, file)).isFile());
+  console.log('contents dirs', dirs);
+  console.log('contents files', files);
   
   const dirPath = path.join(process.cwd(), directory);
   return fs.readdirSync(dirPath).filter(file => file.endsWith('.jpeg') || file.endsWith('.jpg') || file.endsWith('.png'));
