a487fbb330adc0126cc5ae2a8eaf85bbec88e753
ElvarHelga <elvarhelga@gmail.com>
2025-01-18 16:58:49 +0000

    Now logging contents of cwd

--- lib/getFilenames/getFilenames.jsx
+++ lib/getFilenames/getFilenames.jsx
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 
 export function getFilenames(directory) {
-  console.log('directory', directory);
   console.log('process.cwd()', process.cwd());
+  console.log('contents', fs.readdirSync(process.cwd()).filter(file => fs.lstatSync(path.join(process.cwd(), file)).isFile()));
   const dirPath = path.join(process.cwd(), directory);
   return fs.readdirSync(dirPath).filter(file => file.endsWith('.jpeg') || file.endsWith('.jpg') || file.endsWith('.png'));
 }
