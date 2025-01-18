a4ca7c6b9e829bb42855982f679bd38f992b9745
ElvarHelga <elvarhelga@gmail.com>
2025-01-18 17:19:45 +0000

    Zoning in on change causing build fail

--- lib/getFilenames/getFilenames.jsx
+++ lib/getFilenames/getFilenames.jsx
@@ -5,11 +5,11 @@ export function getFilenames(directory) {
   console.log('process.cwd()', process.cwd());
   var dirs = fs.readdirSync(process.cwd()).filter(file => fs.lstatSync(path.join(process.cwd(), file)).isDirectory());
   dirs.forEach(dir => {
-    if(dir !== 'node_modules') {
+    //if(dir !== 'node_modules') {
       var d_path = path.join(process.cwd(), dir);
       console.log('dir', dir);
       console.log('contents', fs.readdirSync(d_path).filter(file => fs.lstatSync(path.join(d_path, file)).isDirectory()));
-    }
+    //}
   });  
   const dirPath = path.join(process.cwd(), directory);
   return fs.readdirSync(dirPath).filter(file => file.endsWith('.jpeg') || file.endsWith('.jpg') || file.endsWith('.png'));
