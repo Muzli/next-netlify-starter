2aa6b4fa5b6497526a87e63de96367e098e9c95e
ElvarHelga <elvarhelga@gmail.com>
2025-01-18 17:22:47 +0000

    Reverting array iterator

--- lib/getFilenames/getFilenames.jsx
+++ lib/getFilenames/getFilenames.jsx
@@ -4,13 +4,8 @@ import path from 'path';
 export function getFilenames(directory) {
   console.log('process.cwd()', process.cwd());
   var dirs = fs.readdirSync(process.cwd()).filter(file => fs.lstatSync(path.join(process.cwd(), file)).isDirectory());
-  dirs.forEach(dir => {
-    //if(dir !== 'node_modules') {
-      var d_path = path.join(process.cwd(), dir);
-      console.log('dir', dir);
-      console.log('contents', fs.readdirSync(d_path).filter(file => fs.lstatSync(path.join(d_path, file)).isDirectory()));
-    //}
-  });  
+  console.log('contents', dirs);
+  
   const dirPath = path.join(process.cwd(), directory);
   return fs.readdirSync(dirPath).filter(file => file.endsWith('.jpeg') || file.endsWith('.jpg') || file.endsWith('.png'));
 }
