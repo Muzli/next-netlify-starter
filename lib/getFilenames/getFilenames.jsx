e9edb1f30c0df045ec76b40bcb6f456a3ccc406c
ElvarHelga <elvarhelga@gmail.com>
2025-01-19 11:34:29 +0000

    Now working from root. Excluding big directories of no help

--- lib/getFilenames/getFilenames.jsx
+++ lib/getFilenames/getFilenames.jsx
@@ -2,17 +2,19 @@ import fs from 'fs';
 import path from 'path';
 
 function logDirectories(base_dir) {
-  var dirs = fs.readdirSync(base_dir).filter(file => fs.lstatSync(path.join(base_dir, file)).isDirectory());
-  console.log("base_dir", base_dir);
-  console.log("dirs", dirs);
-  dirs.forEach((dir) => {
-    logDirectories(path.join(base_dir, dir));
-  });
+  if(base_dir !== 'node_modules' && base_dir !== 'vendor') {
+    var dirs = fs.readdirSync(base_dir).filter(file => fs.lstatSync(path.join(base_dir, file)).isDirectory());
+    console.log("base_dir", base_dir);
+    console.log("dirs", dirs);
+    dirs.forEach((dir) => {
+      logDirectories(path.join(base_dir, dir));
+    });
+  }
 }
 
 export function getFilenames(directory) {
   console.log('process.cwd()', process.cwd());
-  var rootDirPath = path.join(__dirname, '..', '..');
+  var rootDirPath = path.join(process.cwd(), '..', '..');
   var dirs = fs.readdirSync(rootDirPath).filter(file => fs.lstatSync(path.join(rootDirPath, file)).isDirectory());
   console.log('root dirs', dirs);
   dirs.forEach((dir) => { logDirectories(path.join(rootDirPath, dir)); });
